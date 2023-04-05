57309661a5cefdc6d31a708fbd6f57a0c670bb61
Liberty Emerson <115541730+libertyskies@users.noreply.github.com>
2023-04-05 12:50:12 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,18 +1,29 @@
 import logo from './cateyes.webp';
 import img1 from './cat1.jpg';
+import React from 'react';
 import img from './cat2.jpg';
 import './App.css';
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Pet Match</h1>
+        <h1>New Kitten Checklist</h1>
+       <div>
+        <List></List>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         What pet should you get?
+         Congratulations on a new kitten!
         </p>
         <img src={img1} className="cat-pic" id="left-cat" alt="cat" />
+        <div class="container">
+          <div class="item"><a>Home</a></div>
+          <div class="item"><a>Cat Match</a></div>
+          <div class="item"><a>FAQ</a></div>
+        </div>
+        <br></br>
         <img src={img} className="cat-pic" id="right-cat" alt="cat" />
         <a
           className="rspca"
