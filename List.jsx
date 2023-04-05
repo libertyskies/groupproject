59506dd0e2c7685ef81af18d01b6481be57e2312
Liberty Emerson <115541730+libertyskies@users.noreply.github.com>
2023-04-05 12:48:20 +0100

    Add files via upload

---
+++ List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function List() {
+    return <ul>
+        <li>Collar</li>
+        <li>Bed</li>
+        <li>Food</li>
+        <li>Water Bowl</li>
+        <li>Scratching Post</li>
+        <li>Tag</li>
+    </ul>
+};
+
+export default List;
